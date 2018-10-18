3ba2886419c5c50274b485615cff744c4829159b
Nikita Evstigneev <nikita.evstigneev@sabo-gmbh.de>
2018-10-18 16:58:59 +0200

    Added more comments

--- src/app/components/Shoutboard.tsx
+++ src/app/components/Shoutboard.tsx
@@ -6,6 +6,13 @@ import * as React from 'react'
  *  - list of submitted messages ordered descending by creation date (newest first)
  * 
  * Any message submitted via form will be displayed in list of messages below.
+ * 
+ * Try to:
+ *  - Use material design components (https://material-ui.com/getting-started/usage/)
+ *      - For example: TextField, Button, Typography, ...
+ *  - For styling use tailwind css classes (https://tailwindcss.com/docs/what-is-tailwind)
+ *      - margins (m-0, mt-2, mx-4, ...)
+ *      - flexbox (flex, flex-row-reverse, ...)
  */
 
 export class Shoutboard extends React.Component<{}, {}> {
